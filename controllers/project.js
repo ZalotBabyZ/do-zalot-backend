6b508a56f2baf9beb0cab0a1e07f548d0d7c1fbc
ZalotBabyZ <babyzalot@gmail.com>
2020-12-17 18:47:53 +0700

    get project for all

--- controllers/project.js
+++ controllers/project.js
@@ -1,4 +1,5 @@
 const db = require('../models');
+// const sequelize = require('sequelize');
 
 const getProject = async (req, res) => {
   try {
@@ -9,17 +10,58 @@ const getProject = async (req, res) => {
       return res.status(400).send({ message: 'Project ID not exist' });
     }
 
+    const isMember = await db.Project.findOne({
+      where: { id: projectId },
+      include: { model: db.Team, where: { user_id: req.user.id, user_status: 'MEMBER' } },
+    });
+
+    if (!isMember) {
+      const project = await db.Project.findOne({
+        where: { id: projectId },
+        attributes: [['project_name', 'name'], 'description', ['vip_until', 'vip'], ['created_at', 'createdAt']],
+        include: {
+          model: db.Team,
+          attributes: [
+            ['user_status', 'userStatus'],
+            ['user_role', 'userRole'],
+            ['created_at', 'createdAt'],
+          ],
+          include: { model: db.User, attributes: ['username', 'image', ['user_color', 'color']] },
+        },
+      });
+
+      return res.status(200).send({ project });
+    }
+
     const projects = await db.Project.findOne({
       where: { id: projectId },
+      attributes: [['project_name', 'name'], 'description', ['vip_until', 'vip'], ['created_at', 'createdAt']],
       include: [
-        { model: db.Team, where: { user_id: req.user.id, user_status: 'MEMBER' } },
+        {
+          model: db.Team,
+          attributes: [
+            ['user_status', 'userStatus'],
+            ['user_role', 'userRole'],
+            ['created_at', 'createdAt'],
+          ],
+          include: { model: db.User, attributes: ['username', 'image', ['user_color', 'color']] },
+        },
         { model: db.Config },
         {
           model: db.Box,
-          attribute: ['id', 'box_name', 'description', 'type', 'box_color', 'project_pin', 'order'],
+          attributes: [
+            'id',
+            ['box_name', 'name'],
+            'description',
+            'type',
+            ['box_color', 'color'],
+            ['project_pin', 'projectPin'],
+            'order',
+          ],
           include: {
             model: db.List,
             attributes: [
+              ['box_id', 'boxId'],
               'id',
               'list',
               'description',
@@ -27,34 +69,32 @@ const getProject = async (req, res) => {
               'status',
               'important',
               'score',
-              'project_pin',
+              ['project_pin', 'projectPin'],
               'order',
-              'list_deadline',
-              'created_at',
-              'updated_at',
+              ['list_deadline', 'listDeadline'],
+              ['created_at', 'createdAt'],
+              ['updated_at', 'updatedAt'],
             ],
-            include: { model: db.Assign, attributes: ['id', 'user_status', 'updated_at'] },
+            include: {
+              model: db.Assign,
+              attributes: ['id', ['user_status', 'userStatus'], ['updated_at', 'updatedAt']],
+            },
           },
         },
       ],
     });
-
-    const project = projects.dataValues;
     const config = projects.Config.dataValues;
-    const team = projects.Teams[0].dataValues;
-    const boxes = projects.Boxes;
+    const user = isMember.Teams[0].dataValues;
+
     // project
-    const projectName = project.project_name;
-    const projectDescription = project.description;
-    const projectVip = project.vip_until;
-    const projectCreate = project.created_at;
+    const { name, description, vip, createdAt, Teams, Boxes } = projects.dataValues;
     // const projectCreate = project.created_at.toISOString().slice(0, 10);
-    const projectColor = config.project_color;
-    const projectDeadLine = config.project_deadline;
+    const color = config.project_color;
+    const deadline = config.project_deadline;
 
     // user
-    const userRole = team.user_role;
-    const JoinAt = team.updated_at;
+    const role = user.user_role;
+    const joinAt = user.updated_at;
     // const JoinAt = team.updated_at.toISOString().slice(0, 10);
 
     // user right
@@ -67,11 +107,17 @@ const getProject = async (req, res) => {
     const approve =
       config.approve_by_a === req.user.id || config.approve_by_b === req.user.id || config.approve_by_c === req.user.id;
 
+    // boxes
+    // const boxes = Boxes.map((box) => {
+    //   const { id, name, description, type, color, projectPin, order, Lists } = box.dataValues;
+    //   return { id, name, description, type, color, projectPin, order, lists: Lists.dataValues };
+    // });
     res.status(200).send({
-      boxes,
-      user: { userRole, JoinAt },
-      project: { projectName, projectDescription, projectVip, projectCreate, projectColor, projectDeadLine },
+      project: { name, description, vip, createdAt, color, deadline },
+      user: { role, joinAt },
       right: { deleteProject, editScore, editConfig, editAssign, regressStatus, expelMember, approve },
+      teams: Teams,
+      boxes: Boxes,
     });
   } catch (err) {
     console.log(err);
